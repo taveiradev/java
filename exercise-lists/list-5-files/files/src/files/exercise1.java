04507591c72f6075f92fce47f436db9e5b02585f
Victor Rodrigues <rodrigueseti@outlook.com>
2020-05-01 19:04:07 -0300

    Exercise One Added

---
+++ exercise-lists/list-5-files/files/src/files/exercise1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package files;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author VICTOR
+ */
+public class exercise1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner in = new Scanner(System.in);
+        
+        String text = in.nextLine();
+        
+        System.out.println("Text: " + text);
+    }
+    
+}
